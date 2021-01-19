08edfea2e360d8dc67e254edfd9295bdee615391
Riza Nafis <riza.nafis@gmail.com>
2021-01-19 08:18:52 +0700

    Create Docker Container Replica Fixes #12

--- ansible/roles/web/files/docker-compose-update.sh
+++ ansible/roles/web/files/docker-compose-update.sh
@@ -5,8 +5,8 @@ set -e
 echo "Commencing docker-compose update `date`" >> /var/log/containers/docker-compose-update.log
 # Do a pull then an update
 /usr/bin/docker-compose -f /home/deploy/square_check/docker/docker-compose.prod.yml pull --no-parallel >> /var/log/containers/docker-compose-update.log 2>&1
-/usr/bin/docker-compose -f /home/deploy/square_check/docker/docker-compose.prod.yml up -d >> /var/log/containers/docker-compose-update.log 2>&1
+/usr/bin/docker-compose -f /home/deploy/square_check/docker/docker-compose.prod.yml --compatibility up -d >> /var/log/containers/docker-compose-update.log 2>&1
 echo "Sleeping 10 seconds." >> /var/log/containers/docker-compose-update.log
 sleep 10
-/usr/bin/docker-compose -f /home/deploy/square_check/docker/docker-compose.prod.yml restart >> /var/log/containers/docker-compose-update.log 2>&1
+/usr/bin/docker-compose -f /home/deploy/square_check/docker/docker-compose.prod.yml --compatibility restart >> /var/log/containers/docker-compose-update.log 2>&1
 echo "Finishing docker-compose update `date`" >> /var/log/containers/docker-compose-update.log
